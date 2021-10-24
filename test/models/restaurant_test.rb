d7241d75f1165020610f3f992a7200b16777d349
Chris Oh <seokkyu125@gmail.com>
2021-10-24 16:55:30 -0400

    level 2: restaurant has multiple menus

--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,7 +1,12 @@
-require "test_helper"
+require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Restaurant has multiple menus' do
+    restaurant = Restaurant.create(name: 'Super Cajun Seafood')
+    menu1 = Menu.create(title: 'Appetizers', restaurant_id: restaurant.id)
+    menu2 = Menu.create(title: 'Main Dish', restaurant_id: restaurant.id)
+    menu_ids = restaurant.menus.pluck(:id)
+    assert_includes(menu_ids, menu1.id)
+    assert_includes(menu_ids, menu2.id)
+  end
 end
